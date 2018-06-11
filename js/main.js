7c407331bfb0147f0a0c128f0dcedc49b80d4c55
lukeuni <34680196+lukeuni@users.noreply.github.com>
2018-06-11 21:09:13 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+$(document).ready(function () {
+    "use strict";
+
+    
+    $('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+});
+    
+ $(window).scroll(function() {
+    if ($(window).scrollTop() > 10) {
+        $('#navBar').addClass('floatingNav');
+    } else {
+        $('#navBar').removeClass('floatingNav');
+    }
+});
+    AOS.init();
+});
