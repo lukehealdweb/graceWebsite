a0d7d9dfb305e57de8c0d995d01a877693e9dc7e
lukeuni <34680196+lukeuni@users.noreply.github.com>
2018-06-22 23:56:54 +0300

    new website changes
    compelte update

--- js/main.js
+++ js/main.js
@@ -23,4 +23,40 @@ $(document).ready(function () {
     }
 });
     AOS.init();
+    
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+    
+    $(function() {
+	$('.hamburger-button').on('click', function(event){
+		event.preventDefault();
+		
+		$(this).toggleClass('active');
+		$('.overlay').toggleClass('visible');
+	});
 });
+    
+    
+});
+
+
+
+
+
+
+
